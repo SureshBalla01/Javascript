cc383d89812e7004014575df018486af95c59e56
SureshBalla01 <115235679+SureshBalla01@users.noreply.github.com>
2022-10-12 23:52:44 +0530

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,9 @@
+var x,y;
+
+for(var x=1; x<=6;x++) {
+    for(y=1; y<x; y++) {
+        chr = chr+("*");
+    }
+    console.log(chr);
+    var chr='  ';
+}
---
+++ assignment8.js
@@ -0,0 +1,7 @@
+arr = [43,"what",9,"true","cannot","false","be",3,"true"]
+newarr=[]
+for(i=(arr.length-1);i>-1;i--){
+    newarr.push(arr[i]);
+}
+console.log(newarr);
+
